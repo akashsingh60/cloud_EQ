8f6d3db63221c02e625596731c01c3c77155b6a5
Akash Singh <akashsinghthakur60@gmail.com>
2023-03-30 14:46:49 +0530

    terraform commit

---
+++ terraform/forth.java
@@ -0,0 +1,1 @@
+Ncurrently pursuing CAfrom chandigarh university
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,27 @@
 resource "local_file" "file1" {
-  content  = "terraform seekh rha hu"
   filename = "first.py"
+  content  = "Hello! I am Akash Singhfrom CloudEQ"
+}
+
+resource "local_file" "file2" {
+  filename = "second.html"
+  content  = local.name
+}
+
+resource "local_file" "file3" {
+  content  = random_id.random_values.hex
+  filename = "third.css"
+}
+
+resource "local_file" "file4" {
+  filename = "forth.java"
+  content  = "Ncurrently pursuing CAfrom chandigarh university"
+}
+
+locals {
+  name = "<h>Welcome to CloudEQ</h>"
+}
+
+resource "random_id" "random_values" {
+  byte_length = 8
 }
